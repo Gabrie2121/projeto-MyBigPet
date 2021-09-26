6ff8be2dd05cef0cd057a1cfe28c530a82a5bb58
Gabriel Miranda <gabriel.miranda041200@gmail.com>
2021-09-25 23:21:01 -0300

    Finalizado tela de cadastro funcional

---
+++ app/src/main/java/com/example/mybigpet/MainCadAnimal.java
@@ -0,0 +1,14 @@
+package com.example.mybigpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainCadAnimal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadanimal);
+    }
+}
